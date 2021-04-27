53e606257bb08500fde5536367cf7d336853a3ba
raulsalmeron <a_rausalgra@iestriana.com>
2021-04-27 13:53:41 +0200

    desastre de examen

---
+++ JS01/examen/func2.js
@@ -0,0 +1,9 @@
+function Borde() {    
+    document.getElementById("Borde").className = "oculto";
+    document.getElementById("SinBorde").className = "visible";
+}
+
+function SinBorde() {      
+    document.getElementById("Borde").className = "visible";
+    document.getElementById("Sinborde").className = "oculto";
+}
